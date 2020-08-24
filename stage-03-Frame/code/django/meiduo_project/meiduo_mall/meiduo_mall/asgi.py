20521060e808880cada616d7693d5602081ec3d2
JsonYe <yshye824252187@163.com>
2020-08-24 10:03:01 +0800

    add django project

--- stage-03-Frame/code/django/meiduo_project/meiduo_mall/meiduo_mall/asgi.py
+++ stage-03-Frame/code/django/meiduo_project/meiduo_mall/meiduo_mall/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for meiduo_project project.
+ASGI config for meiduo_mall project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meiduo_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'meiduo_mall.settings')
 
 application = get_asgi_application()
--- stage-03-Frame/code/django/meiduo_project/meiduo_mall/meiduo_mall/urls.py
+++ stage-03-Frame/code/django/meiduo_project/meiduo_mall/meiduo_mall/urls.py
@@ -1,4 +1,4 @@
-"""meiduo_project URL Configuration
+"""meiduo_mall URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
